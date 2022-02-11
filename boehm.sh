fe21ba47170384f88934397fcd91e5fbdfc6ab6e
csae8092 <peter.andorfer@oeaw.ac.at>
2022-02-11 11:42:50 +0100

    böhm arche ingest worfklow #14

--- boehm.sh
+++ boehm.sh
@@ -6,3 +6,7 @@ mkdir boehm_data && mkdir boehm_tei
 tar -xf boehm_dl -C boehm_data
 find -path "*boehm/tei-files/boehm_*.xml" -exec cp -prv '{}' './boehm_tei' ';'
 rm -rf boehm_data && rm -rf boehm_dl
+find ./boehm_tei/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@.jpg"@.tif"@g'
+find ./boehm_tei/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@<div xmlns="http://www.tei-c.org/ns/1.0"@<div@g'
+find ./boehm_tei/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@ xmlns:ko="https://www.kraus.wienbibliothek.at/"@@g'
+find ./boehm_tei/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@<TEI xmlns="http://www.tei-c.org/ns/1.0"@ <TEI xmlns="http://www.tei-c.org/ns/1.0" xmlns:ko="https://www.kraus.wienbibliothek.at/"@g'
