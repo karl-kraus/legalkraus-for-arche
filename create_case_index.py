a0dc987a45ff06ba3b713dec92017a3053203297
csae8092 <peter.andorfer@oeaw.ac.at>
2022-03-10 13:19:30 +0100

    added keywords and class codes #16

--- create_case_index.py
+++ create_case_index.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import glob
 import json
 from acdh_tei_pyutils.tei import TeiReader
 from lxml import etree as ET
 
-
-# In[2]:
-
-
 files = glob.glob('./data/cases_tei/C*.xml')
 tei_ns = {'tei': "http://www.tei-c.org/ns/1.0"}
-
-
-# In[4]:
-
-
 faulty = []
 cases = []
+class_codes = {}
+keywords = []
 persons = {}
 roles = {}
 for x in files:
@@ -34,6 +24,15 @@ for x in files:
     case['title'] = doc.any_xpath('.//tei:title/text()')[0]
     case['docs'] = [x.text for x in doc.any_xpath('.//tei:list[1]/tei:item/tei:ref')]
     case['docs_count'] = len(case['docs'])
+    case['keywords'] = []
+    for t in doc.any_xpath('.//tei:term/text()'):
+       case['keywords'].append(t)
+       keywords.append(t)
+    for c in doc.any_xpath('.//tei:classCode'):
+        cc = {}
+        cc['id'] = c.attrib['scheme']
+        cc['label'] = c.text
+        class_codes[c.attrib['scheme']] = c.text
     actors = []
     for a in doc.any_xpath('.//tei:person[@role]'):
         actor = {}
@@ -50,43 +49,9 @@ for x in files:
 data = {
     'cases': cases,
     'persons': persons,
-    'roles': roles
+    'roles': roles,
+    'keywords': list(set(keywords)),
+    'class_codes': class_codes
 }
-
-
-# In[5]:
-
-
-import json
 with open('cases-index.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False)
-
-
-# In[ ]:
-
-
-for a in actors:
-    actor = {}
-    actor['id'] = a.xpath('./@ref')[0]
-    actor['title'] = a.xpath('./tei:persName/text()', namespaces=tei_ns)[0]
-    actor['role_id'] = a.xpath('@role', namespaces=tei_ns)[0]
-    actor['role_label'] = a.xpath('./tei:ab/text()', namespaces=tei_ns)[0]
-
-
-# In[ ]:
-
-
-actor
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
