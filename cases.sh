87de0d133d1217c119ea67f359bf4ca46c7a24df
csae8092 <peter.andorfer@oeaw.ac.at>
2022-03-11 07:37:51 +0100

    proper acdh-ids for docs mentioned in case TEIs

--- cases.sh
+++ cases.sh
@@ -5,4 +5,5 @@ wget -O cases_dl --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" https://gitlab.com/ap
 mkdir cases_data && mkdir -p data/cases_tei
 tar -xf cases_dl -C cases_data
 find -path "*collections/C_*.xml" -exec cp -prv '{}' './data/cases_tei' ';'
+find ./data/cases_tei/ -type f -name "C_*.xml"  -print0 | xargs -0 sed -i 's@ref target="https://id.acdh.oeaw.ac.at/D_@ref target="https://id.acdh.oeaw.ac.at/legalkraus/D_@g'
 rm -rf cases_data && rm -rf cases_dl
