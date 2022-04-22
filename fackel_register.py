58d4e30a71676357fe736eddfa4e2d36aaefc92e
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-22 17:00:02 +0200

    ohm!!!

--- fackel_register.py
+++ fackel_register.py
@@ -1,23 +1,24 @@
 import glob
-
+import collections
 import pandas as pd
-from lxml import etree as ET
-
 from acdh_tei_pyutils.tei import TeiReader
+from lxml import etree as ET
+from collections import defaultdict
 from utils import gsheet_to_df, G_SHEET_FACKEL
 
+df = gsheet_to_df(G_SHEET_FACKEL)
+
+df[['p_from', 'p_to']] = df['pages'].str.split('–', 1, expand=True)
+
 def full_idno(row):
-    if not pd.isna(row['p_to']):
-        corresp = f"{row['idno']},{int(row['p_to']):03}"
+    if not pd.isna(row['p_from']):
+        corresp = f"{row['idno']},{int(row['p_from']):03}"
     else:
         corresp = f"{row['idno']}"
-    return corresp
+    return "".join(corresp.split())
 
-
-df = gsheet_to_df(G_SHEET_FACKEL)
-df[['p_from', 'p_to']] = df['pages'].str.split('–', 1, expand=True)
-df['p_to'] = df['p_to'].fillna(df['p_from'])
 df['corresp'] = df.apply (lambda row: full_idno(row), axis=1)
+df['p_to'] = df['p_to'].fillna(df['p_from'])
 fackel_doc = TeiReader('./data/indices/listfackel.xml')
 for x in fackel_doc.any_xpath('.//tei:listBibl/tei:bibl'):
     x.getparent().remove(x)
@@ -28,7 +29,7 @@ for i, row in df.iterrows():
         corresp = f"{row['idno']},{int(row['p_to']):03}"
     else:
         corresp = f"{row['idno']}"
-    bibl.attrib['corresp'] = "".join(corresp.split())
+    bibl.attrib['corresp'] = row['corresp']
     bibl.attrib['type'] = 'fackel'
     title = ET.Element("{http://www.tei-c.org/ns/1.0}title")
     title.attrib['level'] = 's'
@@ -66,25 +67,49 @@ for i, row in df.iterrows():
         pages.attrib['from'] = row['p_from']
         pages.attrib['to'] = row['p_to']
         bibl.append(pages)
-    
 
 with open('hans.xml', 'w', encoding='utf-8') as f:
     f.write(ET.tostring(list_bibl_node, pretty_print=True, encoding='utf-8').decode('utf-8'))
 
+new_df = df[~df["idno"].str.contains(',')]
+new_df[['p_to', 'p_from']] = new_df[['p_to', 'p_from']].astype('int')
 files = glob.glob('../data/objects/D_*.xml')
-fackel_refs = set()
+idnos = set(df['idno'].values)
+fackel_refs = defaultdict(set)
 for x in files:
     try:
         doc = TeiReader(x)
     except:
         continue
+    xml_id = x.split('/')[-1]
     for ref in doc.any_xpath('.//tei:rs[@subtype="fackel"]/@ref'):
-        fackel_refs.add(ref)
-fackel_pages = {}
-for x in fackel_refs:
-    page = x.split(',')[-1]
+        fid, value = ref.split(',')
+        try:
+            match = new_df.query(
+                    f'idno=="{fid}" and p_from<={int(value)} and p_to>={int(value)}'
+                )['corresp'].values[0]
+        except:
+            if ref in idnos:
+                match = ref
+            else:
+                "NO MATCH"
+        try:
+            fackel_refs[match].add(f"{xml_id}__{ref}")
+        except:
+            pass
+ref_lookup = collections.OrderedDict(sorted(fackel_refs.items()))
+fackel_doc = TeiReader('./hans.xml')
+
+for x in fackel_doc.any_xpath('.//tei:bibl'):
+    corresp = x.attrib['corresp']
     try:
-        int(page)
+        match = ref_lookup[corresp]
     except:
         continue
-    fackel_pages[x] = int(page)
+    for y in match:
+        ref = ET.Element("{http://www.tei-c.org/ns/1.0}ref")
+        ref.attrib['target'] = y.split('__')[1]
+        ref.text = f"{y.split('__')[0]}"
+        x.append(ref)
+   
+fackel_doc.tree_to_file('sums.xml')
