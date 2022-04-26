8b6ddacdc0b7d4840a64b1e636bc1d51cf23eb16
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-26 19:10:33 +0200

    local ingest with delete cmds

--- local_ingest.sh
+++ local_ingest.sh
@@ -3,3 +3,7 @@ vendor/bin/arche-import-metadata html/arche.rdf http://127.0.0.1/api username pa
 vendor/bin/arche-import-metadata html/arche-cases2.rdf http://127.0.0.1/api username password --retriesOnConflict 25
 vendor/bin/arche-import-metadata html/arche-boehm.rdf http://127.0.0.1/api username password --retriesOnConflict 25
 vendor/bin/arche-import-metadata html/arche-indices.rdf http://127.0.0.1/api username password --retriesOnConflict 25
+
+# vendor/bin/arche-delete-resource https://id.acdh.oeaw.ac.at/legalkraus/title-img.jpg  http://127.0.0.1/api username password --recursively
+# vendor/bin/arche-delete-resource https://id.acdh.oeaw.ac.at/legalkraus  http://127.0.0.1/api username password --recursively
+# vendor/bin/arche-delete-resource https://id.acdh.oeaw.ac.at/pub-boehm1995 http://127.0.0.1/api username password --recursively
